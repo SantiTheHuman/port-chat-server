a498657efce1020a69552e6155089ddab7dfec6b
Santi Garcia <58360969+santosuke@users.noreply.github.com>
2021-02-18 16:29:41 +0100

    add option to change user email or password

--- models/user.js
+++ models/user.js
@@ -28,24 +28,18 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-UserSchema.pre("save", function (next) {
-  var user = this;
-  if (user.password) {
-    if (!user.isModified("password")) return next();
+UserSchema.pre("findOneAndUpdate", function (next) {
+  const user = this._update;
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) return next(err);
 
-    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    bcrypt.hash(user.password, salt, function (err, hash) {
       if (err) return next(err);
 
-      bcrypt.hash(user.password, salt, function (err, hash) {
-        if (err) return next(err);
-
-        user.password = hash;
-        next();
-      });
+      user.password = hash;
+      next();
     });
-  } else {
-    next();
-  }
+  });
 });
 
 UserSchema.statics.authenticate = function (username, password, callback) {
